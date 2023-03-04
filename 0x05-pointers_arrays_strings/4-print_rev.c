84ebb54dce6c598edd3654e7e269537f5f994198
Danielle Badobre <badobredanielle@gmail.com>
2023-03-04 20:22:02 +0000

    prints string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+		n++;
+	while (n >= 0)
+	{
+		_putchar(s[n]);
+		n--;
+	}
+	_putchar('\n');
+}
